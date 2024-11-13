c1117874452848a855177806bff9c4c66c699715
Daniel Christoph <d4niel.christoph@gmail.com>
2024-11-13 21:04:35 +0100

    fix future warning

--- model_training_setups/GPT2/train.py
+++ model_training_setups/GPT2/train.py
@@ -1,4 +1,3 @@
-import torch
 from transformers import (
     AutoConfig,
     GPT2LMHeadModel,
@@ -9,8 +8,10 @@ from transformers import (
 )
 from datasets import load_dataset, DatasetDict, load_from_disk
 
+import torch
 # torch.cuda.set_device('cuda:0')
 
+output = "./gpt2-wikipedia"
 # Load the dataset
 dataset = load_dataset("wikimedia/wikipedia", "20231101.en")
 
@@ -18,7 +19,6 @@ dataset = load_dataset("wikimedia/wikipedia", "20231101.en")
 context_length = 128
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token  # Set pad token
-
 
 def tokenize(element):
     # Tokenize input, handling long sequences with `return_overflowing_tokens`
@@ -44,13 +44,11 @@ def tokenize(element):
 # Tokenize the entire dataset and remove unnecessary columns
 tokenized_datasets = dataset.map(tokenize, batched=True, remove_columns=dataset["train"].column_names)
 
-# Load the previously saved tokenized dataset
-# tokenized_datasets = load_from_disk('wikipedia_20231101_en/tokenized_ds')
-
 # Save the tokenized dataset (optional)
 tokenized_datasets.save_to_disk("wikipedia_20231101_en/tokenized_ds")
 
-tokenized_datasets.set_format("torch")
+# Load the previously saved tokenized dataset and ignore the above steps for the dataset processing
+# tokenized_datasets = load_from_disk('wikipedia_20231101_en/tokenized_ds')
 
 # Split into train and eval (e.g., 90% train, 10% eval)
 train_test_split = tokenized_datasets["train"].train_test_split(test_size=0.1, seed=42)
@@ -73,8 +71,12 @@ model = GPT2LMHeadModel(config)
 
 # Training arguments
 training_args = TrainingArguments(
-    output_dir="./gpt2-wikipedia",  # Output directory
+    output_dir=output,  # Output directory
     per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    eval_strategy="steps",
+    eval_steps=5_000,
+    logging_steps=5_000,
     gradient_accumulation_steps=8,
     num_train_epochs=3,
     weight_decay=0.1,
@@ -82,10 +84,7 @@ training_args = TrainingArguments(
     lr_scheduler_type="cosine",
     learning_rate=5e-4,
     save_steps=5_000,
-    logging_steps=5_000,
-    eval_strategy="steps",  # Enable evaluation during training
-    eval_steps=2_500,  # Evaluate every 2,500 steps
-    fp16=True,  # Use mixed precision for faster training and reduced memory
+    fp16=True
 )
 
 # Initialize the Trainer with both train and eval datasets
@@ -102,6 +101,6 @@ trainer = Trainer(
 trainer.train()
 
 # Save the final model and tokenizer
-model.save_pretrained("./gpt2-wikipedia")
-tokenizer.save_pretrained("./gpt2-wikipedia")
-print("Training complete. Model saved at ./gpt2-wikipedia")
+model.save_pretrained(output)
+tokenizer.save_pretrained(output)
+print(f"Training complete. Model saved at {output}")
