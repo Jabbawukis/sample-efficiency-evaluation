a8c6110bf81e21556594a1e5da0cba567c964c76
Daniel Christoph <d4niel.christoph@gmail.com>
2024-12-10 16:01:20 +0100

    add avg increase in matches stat

--- utility_scripts/alias_importance_measure.py
+++ utility_scripts/alias_importance_measure.py
@@ -40,6 +40,8 @@ matches_with_aliases = {"subj_and_obj": set(), "subj": set(), "obj": set()}
 matches_overall = {"with aliases": set(), "no need for alias": set()}
 no_matches = {"with aliases": set(), "without aliases": set()}
 
+avg_increase_in_matches = {"sum": 0, "count": 0}
+
 for relation_id, facts in match_dict_aliases.items():
     for subj_id, aliases_fact in facts.items():
         no_aliases_fact = match_dict_no_aliases[relation_id][subj_id]
@@ -54,6 +56,8 @@ for relation_id, facts in match_dict_aliases.items():
             else:
                 raise ValueError("Error: matches without aliases in alias dict")
             matches_overall["with aliases"].add((relation_id, subj_id, aliases_fact["obj_id"]))
+            avg_increase_in_matches["sum"] += aliases_fact["occurrences"] - no_aliases_fact["occurrences"]
+            avg_increase_in_matches["count"] += 1
         # matches where the fact with aliases has occurrences, and the fact without aliases has no occurrences
         elif aliases_fact["occurrences"] > no_aliases_fact["occurrences"] == 0:
             if len(aliases_fact["subj_aliases"]) > 0 and len(aliases_fact["obj_aliases"]) > 0:
@@ -66,6 +70,8 @@ for relation_id, facts in match_dict_aliases.items():
                 raise ValueError("Error: matches without aliases in alias dict")
             matches_overall["with aliases"].add((relation_id, subj_id, aliases_fact["obj_id"]))
             no_matches["without aliases"].add((relation_id, subj_id, no_aliases_fact["obj_id"]))
+            avg_increase_in_matches["sum"] += aliases_fact["occurrences"]
+            avg_increase_in_matches["count"] += 1
         # matches where the fact without aliases the same number of occurrences as the fact with aliases
         elif aliases_fact["occurrences"] == no_aliases_fact["occurrences"] > 0:
             matches_overall["no need for alias"].add((relation_id, subj_id, no_aliases_fact["obj_id"]))
@@ -116,3 +122,6 @@ percentage_of_instances_with_matches_where_aliases_mattered = overall_matches_al
 )
 assert overall_matches_aliases == (overall_matches_aliases + overall_matches_no_need_aliases) * percentage_of_instances_with_matches_where_aliases_mattered
 print(f"% of instances overall with more matches due to aliases: {percentage_of_instances_with_matches_where_aliases_mattered}\n")
+
+avg_increase_in_matches = avg_increase_in_matches["sum"] / avg_increase_in_matches["count"]
+print(f"Average increase in matches due to aliases: {avg_increase_in_matches}\n")
