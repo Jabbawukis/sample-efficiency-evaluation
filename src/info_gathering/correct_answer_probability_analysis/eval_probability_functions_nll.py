03702ea61423c8d787a5acbb2e95079b1afedd97
Daniel Christoph <d4niel.christoph@gmail.com>
2025-04-08 14:12:18 +0200

    remove deprecated functions

--- src/info_gathering/correct_answer_probability_analysis/eval_probability_functions_nll.py
+++ src/info_gathering/correct_answer_probability_analysis/eval_probability_functions_nll.py
@@ -9,18 +9,8 @@ from utility.utility import load_json_dict, save_dict_as_json
 from info_gathering.correct_answer_probability_analysis.probability_function_optimization.cdf_nll_optimization import (
     vectorized_cdf,
 )
-
-# from info_gathering.correct_answer_probability_analysis.probability_function_optimization.psf_nll_optimization import (
-#     vectorized_psf,
-# )
-# from info_gathering.correct_answer_probability_analysis.probability_function_optimization.psf_ext_nll_optimization import (
-#     vectorized_psf_ext,
-# )
-# from info_gathering.correct_answer_probability_analysis.probability_function_optimization.psf_ext2_nll_optimization import (
-#     vectorized_psf_ext2,
-# )
-from info_gathering.correct_answer_probability_analysis.probability_function_optimization.psf_ext3_nll_optimization import (
-    vectorized_psf_ext3,
+from info_gathering.correct_answer_probability_analysis.probability_function_optimization.psf_nll_optimization import (
+    vectorized_psf,
 )
 from info_gathering.correct_answer_probability_analysis.probability_function_optimization.util import (
     get_slice_data,
@@ -88,32 +78,8 @@ if __name__ == "__main__":
             "Params": "Lambdas",
             "Param": "lambda",
         },
-        # {
-        #     "function_method": vectorized_psf,
-        #     "function_name": "Power Scaling Function",
-        #     "file_name": "psf_optimized_alphas.json",
-        #     "get_slice_data": get_slice_data,
-        #     "Params": "Alphas",
-        #     "Param": "alpha",
-        # },
-        # {
-        #     "function_method": vectorized_psf_ext,
-        #     "function_name": "Power Scaling Function Extended",
-        #     "file_name": "psf-ext_optimized_alphas.json",
-        #     "get_slice_data": get_slice_data,
-        #     "Params": "Alphas",
-        #     "Param": "alpha",
-        # },
-        # {
-        #     "function_method": vectorized_psf_ext2,
-        #     "function_name": "Power Scaling Function Extended2",
-        #     "file_name": "psf-ext2_optimized_alphas.json",
-        #     "get_slice_data": get_slice_data,
-        #     "Params": "Alphas",
-        #     "Param": "alpha",
-        # },
         {
-            "function_method": vectorized_psf_ext3,
+            "function_method": vectorized_psf,
             "function_name": "Power Scaling Function",
             "file_name": "psf-ext3_optimized_alphas.json",
             "get_slice_data": get_slice_data,
